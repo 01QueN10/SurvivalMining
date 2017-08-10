171141e97380fba7b3df3203c3778458420477b8
QN <01quen10@gmail.com>
2017-08-10 20:32:32 +0900

    Initial commit

---
+++ games/SurvivalMining/mods/ores/init.lua
@@ -0,0 +1,19 @@
+--
+-- User: QN
+-- Date: 2017-08-10
+-- Time: 오전 11:27
+-- To change this template use File | Settings | File Templates.
+--
+
+minetest.register_craftitem("ores:copper_lump", {
+    description = "Copper Lump",
+    inventory_image = "copper_lump.png"
+})
+minetest.register_craftitem("ores:tin_lump", {
+    description = "Tin Lump",
+    inventory_image = "tin_lump.png"
+})
+minetest.register_craftitem("ores:iron_lump", {
+    description = "Iron Lump",
+    inventory_image = ""
+})
